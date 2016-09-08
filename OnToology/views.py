30371bb74c41fc1dc1e2bbf84bb15490020ab61b
Ahmad Alobaid <ahmad88me@gmail.com>
2016-09-08 16:08:02 +0200

    fix no repos error

--- OnToology/views.py
+++ OnToology/views.py
@@ -125,7 +125,8 @@ def home(request):
     repos = Repo.objects.order_by('-last_used')[:10]
     num_of_users = len(User.objects.all())
     num_of_repos = len(Repo.objects.all())
-    last_used = Repo.objects.all().order_by('-last_used')[0].last_used
+    last_used = datetime.now()
+    #last_used = Repo.objects.all().order_by('-last_used')[0].last_used
     #last_used = '%d, %d' % (last_used.month, last_used.year)
     return render(request, 'home.html', {'repos': repos, 'user': request.user, 'num_of_users': num_of_users,
                                          'num_of_repos': num_of_repos, 'last_used': last_used})
